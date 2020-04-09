5047a9cc67fc3bea7aff355a872f633b2818e0c3
Florian Thiery <web@florian-thiery.de>
2020-04-09 17:12:54 +0200

    add more files

---
+++ py/run_AntonineItinerary.py
@@ -0,0 +1,48 @@
+# import dependencies
+import geojson
+from geomet import wkt
+import json
+import uuid
+import pandas as pd
+import os
+import codecs
+import datetime
+
+# set paths
+dir_path = os.path.dirname(os.path.realpath(__file__))
+file_in = dir_path.replace("\\py","\\csv") + "\\" + "AntonineItinerary.csv"
+file_out = dir_path.replace("\\py","\\geojson") + "\\" + "AntonineItinerary.geojson"
+
+# read csv file
+data = pd.read_csv(
+    file_in, # relative python path to subdirectory
+    encoding='utf-8',
+    sep='\t', # deliminiter
+    quotechar="'",  # single quote allowed as quote character
+    usecols=['group','name','roman_name','city','county','grid','long','lat'], # only load the  columns specified
+    skiprows=0, # skip X rows of the file
+    na_values=['.', '??'] # take any '.' or '??' values as NA
+)
+
+features = []
+for index, row in data.iterrows():
+    properties = {}
+    properties['group'] = int(row['group'])
+    properties['name'] = row['name']
+    properties['roman_name'] = row['roman_name']
+    properties['city'] = row['city']
+    properties['county'] = row['county']
+    properties['grid'] = row['grid']
+    #print(properties)
+    point = "POINT("+str(float(row['long']))+" "+str(float(row['lat']))+")"
+    #print(point)
+    feature = { 'type': 'Feature', 'properties': properties, 'geometry': wkt.loads(point) }
+    print(feature)
+    features.append(feature)
+
+geojson = {'type': 'FeatureCollection', 'features': features }
+
+print(json.dumps(geojson, sort_keys=True, indent=4))
+
+with open(file_out, 'w') as f:
+    json.dump(geojson, f)
